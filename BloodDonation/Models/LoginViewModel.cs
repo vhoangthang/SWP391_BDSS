35988958ee4193fdbcc01744205e9bbef87ea012
Le Vu Van <levuvanbat11@gmail.com>
2025-06-23 16:01:47 +0700

    Change Login Interface

--- BloodDonation/Models/LoginViewModel.cs
+++ BloodDonation/Models/LoginViewModel.cs
@@ -11,7 +11,6 @@ namespace BloodDonation.Models
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [Required(ErrorMessage = "Vui lòng chọn loại người dùng")]
         public string Role { get; set; }
 
         public bool RememberMe { get; set; }
