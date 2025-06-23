37896836a23d023d22230218a65569e8151e2708
Le Vu Van <levuvanbat11@gmail.com>
2025-06-23 16:43:42 +0700

    Modify HomeController

--- BloodDonation/Controllers/HomeController.cs
+++ BloodDonation/Controllers/HomeController.cs
@@ -1,27 +1,29 @@
 using System.Diagnostics;
 using BloodDonation.Models;
 using Microsoft.AspNetCore.Mvc;
+using BloodDonation.Data;
 
 namespace BloodDonation.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AppDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            var username = HttpContext.Session.GetString("Username");
+            ViewBag.IsLoggedIn = !string.IsNullOrEmpty(username);
+            ViewBag.Username = username;
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
