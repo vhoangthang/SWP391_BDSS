0dc4f8cc569f9d10403aff9fcef9b634122d5345
Quản Bá Thành - SE180150 <thanhqbse180150@fpt.edu.vn>
2025-06-23 14:20:17 +0700

    add Repository

---
+++ BloodDonation/Repository/DonorRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BloodDonation.Data;
+using BloodDonation.Models;
+using BloodDonation.Repositories.Interfaces;
+
+namespace BloodDonation.Repositories
+{
+    public class DonorRepository : IDonorRepository
+    {
+        private readonly MyDbContext _context;
+
+        public DonorRepository(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Donor>> GetAllAsync()
+        {
+            return await _context.Donors.ToListAsync();
+        }
+
+        public async Task<Donor> GetByIdAsync(int id)
+        {
+            return await _context.Donors.FindAsync(id);
+        }
+
+        public async Task AddAsync(Donor donor)
+        {
+            _context.Donors.Add(donor);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Donor donor)
+        {
+            _context.Donors.Update(donor);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var donor = await _context.Donors.FindAsync(id);
+            if (donor != null)
+            {
+                _context.Donors.Remove(donor);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ BloodDonation/Repository/Interface/IDonorRepository.cs
@@ -0,0 +1,15 @@
+﻿using BloodDonation.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BloodDonation.Repositories.Interfaces
+{
+    public interface IDonorRepository
+    {
+        Task<IEnumerable<Donor>> GetAllAsync();
+        Task<Donor> GetByIdAsync(int id);
+        Task AddAsync(Donor donor);
+        Task UpdateAsync(Donor donor);
+        Task DeleteAsync(int id);
+    }
+}
