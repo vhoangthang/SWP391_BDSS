3370c68bcdd08afa378b4b4bcb24ad2c0e262d1f
Le Vu Van <levuvanbat11@gmail.com>
2025-06-24 18:46:29 +0700

    Remodify donationSummary

--- BloodDonation/Models/DonationSummaryViewModel.cs
+++ BloodDonation/Models/DonationSummaryViewModel.cs
@@ -4,7 +4,7 @@ namespace BloodDonation.Models
 {
     public class DonationSummaryViewModel
     {
-        public DonationAppointment Appointments { get; set; }
+        public List<DonationAppointment> Appointments { get; set; }
         public Donor Donor { get; set; }
         public Dictionary<string, string> HealthSurveyAnswers { get; set; }
     }
