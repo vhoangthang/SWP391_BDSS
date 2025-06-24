aef607c70d4c92adb0c0f50172f6b5d09558576f
Le Vu Van <levuvanbat11@gmail.com>
2025-06-24 18:29:11 +0700

    add notificationController

---
+++ BloodDonation/Controllers/NotificationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BloodDonation.Controllers
+{
+    public class NotificationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
