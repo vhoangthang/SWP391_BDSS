9f88aa4ff12819746420fec3447b7af4406da9a7
Le Vu Van <levuvanbat11@gmail.com>
2025-06-26 15:15:06 +0700

    Fix BloodRequest in MC view

--- BloodDonation/Models/BloodRequestViewModel.cs
+++ BloodDonation/Models/BloodRequestViewModel.cs
@@ -6,16 +6,6 @@ namespace BloodDonation.Models
 {
     public class BloodRequestViewModel
     {
-        [Required(ErrorMessage = "Vui lòng nhập họ và tên.")]
-        public string PatientName { get; set; }
-
-        public int Age { get; set; }
-
-        public string PhoneNumber { get; set; }
-
-        public string Gender { get; set; }
-
-        public string NationalId { get; set; }
 
         [Required(ErrorMessage = "Vui lòng chọn nhóm máu.")]
         public string BloodType { get; set; }
@@ -24,7 +14,7 @@ namespace BloodDonation.Models
         public decimal Quantity { get; set; }
 
         public string Compatibility { get; set; }
-
+        
         public string Reason { get; set; }
 
         public string ReceptionType { get; set; }
