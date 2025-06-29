b99b9bad08bc02decd4561c6066c5217df7a4aa2
Le Vu Van <levuvanbat11@gmail.com>
2025-06-30 00:46:16 +0700

    Add Notification field into DBcontext

--- BloodDonation/Data/AppDbContext.cs
+++ BloodDonation/Data/AppDbContext.cs
@@ -26,6 +26,7 @@ namespace BloodDonation.Data
 
         public DbSet<HealthSurvey> HealthSurveys { get; set; }
 
+        public DbSet<Notification> Notifications { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
