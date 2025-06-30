55718e1fd70c29a7e8537c14b0cf66ae6cc5901b
Le Vu Van <levuvanbat11@gmail.com>
2025-06-30 23:47:00 +0700

    Decorate notification function

--- BloodDonation/Controllers/NotificationController.cs
+++ BloodDonation/Controllers/NotificationController.cs
@@ -26,5 +26,28 @@ namespace BloodDonation.Controllers
             var notifications = donor.Notifications.OrderByDescending(n => n.SentAt).ToList();
             return View(notifications);
         }
+
+        [HttpPost]
+        public IActionResult MarkAsRead(int id)
+        {
+            var username = HttpContext.Session.GetString("Username");
+            if (string.IsNullOrEmpty(username))
+                return Json(new { success = false, message = "Chưa đăng nhập" });
+
+            var donor = _context.Donors.Include(d => d.Notifications).Include(d => d.Account).FirstOrDefault(d => d.Account.Username == username);
+            if (donor == null)
+                return Json(new { success = false, message = "Không tìm thấy người dùng" });
+
+            var notification = donor.Notifications.FirstOrDefault(n => n.NotificationID == id);
+            if (notification == null)
+                return Json(new { success = false, message = "Không tìm thấy thông báo" });
+
+            if (!notification.IsRead)
+            {
+                notification.IsRead = true;
+                _context.SaveChanges();
+            }
+            return Json(new { success = true });
+        }
     }
 }
