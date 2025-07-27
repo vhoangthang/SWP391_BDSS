7c059a345b63e27fae37ce2761d28b47f989f48a
Hoang Thang <thangvhse182121@fpt.edu.vn>
2025-07-28 04:14:46 +0700

    Optimize AccountController

--- BloodDonation/Controllers/AccountController.cs
+++ BloodDonation/Controllers/AccountController.cs
@@ -3,21 +3,23 @@ using BloodDonation.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Mail;
 using System.Net;
-using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 namespace BloodDonation.Controllers
 {
     public class AccountController : Controller
     {
         private readonly AppDbContext _context;
+        private readonly SmtpSettings _smtpSettings;
         private static string sentCode = "";
         private static string resetCode = "";
         private static string resetEmail = "";
 
-        public AccountController(AppDbContext context)
+        public AccountController(AppDbContext context, IOptions<SmtpSettings> smtpOptions)
         {
             _context = context;
+            _smtpSettings = smtpOptions.Value;
         }
 
         [HttpGet]
@@ -52,6 +54,7 @@ namespace BloodDonation.Controllers
                 var vm = new LoginRegisterViewModel { Register = model, Login = new LoginViewModel() };
                 return View("~/Views/Login/Index.cshtml", vm);
             }
+
             if (_context.Accounts.Any(a => a.Email == model.Email))
             {
                 ViewBag.ShowRegister = true;
@@ -63,11 +66,12 @@ namespace BloodDonation.Controllers
             var account = new Account
             {
                 Username = model.Username,
-                Password = model.Password, 
+                Password = model.Password,
                 Email = model.Email,
-                Role = "Donor", 
-                PermissionLevel = 1 
+                Role = "Donor",
+                PermissionLevel = 1
             };
+
             _context.Accounts.Add(account);
             await _context.SaveChangesAsync();
 
@@ -81,18 +85,26 @@ namespace BloodDonation.Controllers
             string email = request.Email;
             sentCode = new Random().Next(100000, 999999).ToString();
 
-            var smtpClient = new SmtpClient("smtp.gmail.com")
+            var smtpClient = new SmtpClient(_smtpSettings.Host)
             {
-                Port = 587,
-                Credentials = new NetworkCredential("ngvhuy1612@gmail.com", "avobgwpxoctsanga"),
-                EnableSsl = true,
+                Port = _smtpSettings.Port,
+                Credentials = new NetworkCredential(_smtpSettings.UserName, _smtpSettings.Password),
+                EnableSsl = _smtpSettings.EnableSsl,
             };
 
             var mailMessage = new MailMessage
             {
-                From = new MailAddress("ngvhuy1612@gmail.com"),
-                Subject = "Mã xác minh đăng ký",
-                Body = $"Mã xác minh của bạn là: {sentCode}",
+                From = new MailAddress(_smtpSettings.UserName, _smtpSettings.DisplayName),
+                Subject = "Xác minh đăng ký tài khoản Donor",
+                Body = $"Xin chào! \r\n" +
+                $"Blood Donation Support System (Team 7 - SWP391 - SE1837) đã nhận yêu cầu đăng ký tài khoản của bạn.\r\n" +
+                $"\r\n" +
+                $"Mã xác minh của bạn là: {sentCode}\r\n" +
+                $"\r\n" +
+                $"Đây là thư tự động, vui lòng không trả lời\r\n" +
+                $"Trân trọng!\r\n" +
+                $"Blood Donation Support System\r\n" +
+                $"(Team 7 - SWP391 - SE1837)\r\n",
                 IsBodyHtml = false,
             };
 
@@ -105,7 +117,6 @@ namespace BloodDonation.Controllers
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Lỗi gửi email: " + ex.Message);
                 return Json(new { success = false, message = "Gửi mã thất bại: " + ex.Message });
             }
         }
@@ -122,20 +133,29 @@ namespace BloodDonation.Controllers
             resetEmail = email;
             resetCode = new Random().Next(100000, 999999).ToString();
 
-            var smtpClient = new SmtpClient("smtp.gmail.com")
+            var smtpClient = new SmtpClient(_smtpSettings.Host)
             {
-                Port = 587,
-                Credentials = new NetworkCredential("ngvhuy1612@gmail.com", "avobgwpxoctsanga"),
-                EnableSsl = true,
+                Port = _smtpSettings.Port,
+                Credentials = new NetworkCredential(_smtpSettings.UserName, _smtpSettings.Password),
+                EnableSsl = _smtpSettings.EnableSsl,
             };
 
             var mailMessage = new MailMessage
             {
-                From = new MailAddress("ngvhuy1612@gmail.com"),
-                Subject = "Mã xác minh đặt lại mật khẩu",
-                Body = $"Mã xác minh của bạn là: {resetCode}",
+                From = new MailAddress(_smtpSettings.UserName, _smtpSettings.DisplayName),
+                Subject = "Xác minh đặt lại mật khẩu tài khoản Donor",
+                Body = $"Xin chào!\r\n" +
+                $"Blood Donation Support System (Team 7 - SWP391 - SE1837) đã nhận yêu cầu đặt lại mật khẩu tài khoản của bạn.\r\n" +
+                $"\r\n" +
+                $"Mã xác minh của bạn là: {resetCode}\r\n" +
+                $"\r\n" +
+                $"Đây là thư tự động, vui lòng không trả lời\r\n" +
+                $"Trân trọng!\r\n" +
+                $"Blood Donation Support System\r\n" +
+                $"(Team 7 - SWP391 - SE1837)\r\n",
                 IsBodyHtml = false,
             };
+
             mailMessage.To.Add(email);
 
             try
@@ -175,12 +195,10 @@ namespace BloodDonation.Controllers
             return View();
         }
 
-        
         [HttpPost]
         public IActionResult ResetPassword(string newPassword)
         {
             var account = _context.Accounts.FirstOrDefault(a => a.Email == resetEmail);
-
             if (account == null)
             {
                 ViewBag.Error = "Tài khoản không tồn tại.";
@@ -192,7 +210,6 @@ namespace BloodDonation.Controllers
 
             return RedirectToAction("Index", "Login");
         }
-
 
         [HttpGet]
         public IActionResult CheckUsername(string username)
