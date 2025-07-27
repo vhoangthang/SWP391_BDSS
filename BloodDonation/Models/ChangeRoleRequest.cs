b2dceccb927d991ed169598f6285763de6dcaf44
Hoang Thang <thangvhse182121@fpt.edu.vn>
2025-07-28 04:12:33 +0700

    Add Model from Admin and Smtp

---
+++ BloodDonation/Models/ChangeRoleRequest.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BloodDonation.Models
+{
+    public class ChangeRoleRequest
+    {
+        public int id { get; set; }
+        public string newRole { get; set; }
+        public int? medicalCenterId { get; set; }
+    }
+}
